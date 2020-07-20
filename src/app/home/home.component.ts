6b747050e5d75d120f531ae9cdebddc5bc5036bc
Arslan Munir <Arslan Munir>
2020-07-20 10:17:00 +0500

    Added template source code

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  title = '_shared_project';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
